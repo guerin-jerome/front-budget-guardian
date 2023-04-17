4ac80362d14cf5b9051846db52580286336b0e72
Jérôme Guèrin <guerin_jerome@icloud.com>
2023-04-17 19:22:50 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# front-budget-guardian
+
+## Conventions
+
+- [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/)
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <p>Hello world !</p>
+  </React.StrictMode>
+);
