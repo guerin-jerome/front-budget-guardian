2cfddfb01a20c2f04cbc49912f58cddd8f4afde1
Jérôme Guèrin <105017345+guerin-jerome@users.noreply.github.com>
2023-05-12 17:17:48 +0200

    feature(expense-tracking): expense form (#6)

--- src/features/expenseTracking/domain/buildBudgetsImpactedOptions.ts
+++ src/features/expenseTracking/domain/buildBudgetsImpactedOptions.ts
@@ -1,6 +1,15 @@
 import { SelectOption } from "@/common/components/Select/select.type";
 import { Budget } from "@/entities/Budget";
 
+<<<<<<< HEAD
+=======
+export const SELECT_BUDGET_IMPACTED_PLACEHOLDER = {
+  value: "initial_value",
+  text: "Sélectionnez un budget",
+  disabled: true,
+};
+
+>>>>>>> 9862062 (feature(expense-tracking): expense form (#6))
 /**
  * Convertit les valeurs de budgets en options pour un select
  * @param budgets
@@ -8,8 +17,19 @@ import { Budget } from "@/entities/Budget";
  */
 export const buildBudgetsImpactedOptions = (
   budgets: Budget[]
+<<<<<<< HEAD
 ): SelectOption[] =>
   budgets.map(({ id, name }) => ({
     value: id.toString(),
     text: name,
   }));
+=======
+): SelectOption[] => {
+  const newBudgets = budgets.map(({ id, name }) => ({
+    value: id.toString(),
+    text: name,
+  }));
+  newBudgets.unshift(SELECT_BUDGET_IMPACTED_PLACEHOLDER);
+  return newBudgets;
+};
+>>>>>>> 9862062 (feature(expense-tracking): expense form (#6))
