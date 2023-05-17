045b3ba3e15df1e3da2c5458c5ec4b774a3cda27
Jérôme Guèrin <105017345+guerin-jerome@users.noreply.github.com>
2023-05-17 20:21:51 +0200

    project(expense-tracking): user interactions (#14)
    * feat(expense-tracking): mise en place du Budget context + Expense entity + fix test
    
    * feat(expense-tracking): reset current budgets user interaction
    
    * feat(expense-tracking): ajout react hook form
    
    * feat(expense-tracking): expense context + validator form
    
    * feat(expense-tracking): user interaction remove to budget
    
    * fix(expense-tracking): expense mock + tofixed(2)

--- src/features/expenseTracking/domain/buildBudgetsImpactedOptions.ts
+++ src/features/expenseTracking/domain/buildBudgetsImpactedOptions.ts
@@ -1,15 +1,6 @@
 import { SelectOption } from "@/common/components/Select/select.type";
 import { Budget } from "@/entities/Budget";
 
-<<<<<<< HEAD
-=======
-export const SELECT_BUDGET_IMPACTED_PLACEHOLDER = {
-  value: "initial_value",
-  text: "Sélectionnez un budget",
-  disabled: true,
-};
-
->>>>>>> 9862062 (feature(expense-tracking): expense form (#6))
 /**
  * Convertit les valeurs de budgets en options pour un select
  * @param budgets
@@ -17,19 +8,8 @@ export const SELECT_BUDGET_IMPACTED_PLACEHOLDER = {
  */
 export const buildBudgetsImpactedOptions = (
   budgets: Budget[]
-<<<<<<< HEAD
 ): SelectOption[] =>
   budgets.map(({ id, name }) => ({
     value: id.toString(),
     text: name,
   }));
-=======
-): SelectOption[] => {
-  const newBudgets = budgets.map(({ id, name }) => ({
-    value: id.toString(),
-    text: name,
-  }));
-  newBudgets.unshift(SELECT_BUDGET_IMPACTED_PLACEHOLDER);
-  return newBudgets;
-};
->>>>>>> 9862062 (feature(expense-tracking): expense form (#6))
