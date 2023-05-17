f63fd73c8fc112b1ccc41b8d86f573a14dc2c6a3
Jérôme Guèrin <105017345+guerin-jerome@users.noreply.github.com>
2023-05-17 20:21:51 +0200

    project(expense-tracking): user interactions (#14)
    * feat(expense-tracking): mise en place du Budget context + Expense entity + fix test
    
    * feat(expense-tracking): reset current budgets user interaction
    
    * feat(expense-tracking): ajout react hook form
    
    * feat(expense-tracking): expense context + validator form
    
    * feat(expense-tracking): user interaction remove to budget
    
    * fix(expense-tracking): expense mock + tofixed(2)

--- src/features/expenseTracking/hooks/useExpenseForm.ts
+++ src/features/expenseTracking/hooks/useExpenseForm.ts
@@ -24,7 +24,7 @@ export const useExpenseForm = (
   const { setExpenses } = useContext(ExpensesContext);
 
   const budgetsImpactedOptions = buildBudgetsImpactedOptions(budgets);
-  const SubmitTextButtons = buildSubmitTextButtons(isMobileDevice);
+  const submitTextButtons = buildSubmitTextButtons(isMobileDevice);
 
   const handleSubmitExpenseForm: SubmitHandler<ExpenseFormInputs> = (
     inputs: ExpenseFormInputs
