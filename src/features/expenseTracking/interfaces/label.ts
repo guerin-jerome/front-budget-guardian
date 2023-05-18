e04616348e614413de3bc07e3f06a35f32e3e5bc
Jérôme Guèrin <105017345+guerin-jerome@users.noreply.github.com>
2023-05-18 23:44:01 +0200

    refacto(expense-tracking): actions menu + expense form (#17)

--- src/features/expenseTracking/interfaces/label.ts
+++ src/features/expenseTracking/interfaces/label.ts
@@ -7,13 +7,8 @@ export const EXPENSE_DETAILS_LABEL = "Détails";
 export const EXPENSE_BUDGET_IMPACTED_LABEL = "Budget impacté";
 export const EXPENSE_DATE_LABEL = "Date";
 export const EXPENSE_AMOUNT_LABEL = "Montant";
-export const ADD_EXPENSE_TEXT_MOBILE = "Ajouter";
-export const ADD_EXPENSE_TEXT_DESKTOP = "+";
-export const REMOVE_EXPENSE_TEXT_MOBILE = "Retirer";
-export const REMOVE_EXPENSE_TEXT_DESKTOP = "-";
 
 /**
  * Section de la liste des budgets en cours
  */
 export const TOTAL_REMAINING_TITLE_SECTION = "Total restant : ";
-export const RESET_BUTTON_TEXT = "Reset";
