f33f3e0b49abc6605791a5deef13672e763dd445
Jérôme Guèrin <guerin_jerome@icloud.com>
2023-05-22 12:28:12 +0200

    refacto(home): title

--- src/features/home/interfaces/label.ts
+++ src/features/home/interfaces/label.ts
@@ -2,4 +2,5 @@ export const MENU_ITEM_EXPENSES_TRACKING_TEXT_DESKTOP = "Suivi des dépenses";
 export const MENU_ITEM_EXPENSES_TRACKING_TEXT_MOBILE = "Suivi";
 export const MENU_ITEM_BUDGETS_MANAGEMENT_TEXT_DESKTOP = "Gestion des budgets";
 export const MENU_ITEM_BUDGETS_MANAGEMENT_TEXT_MOBILE = "Gestion";
+export const HISTORY_TITLE = "Historique des dépenses";
 export const MENU_ITEM_HISTORIQUE_TEXT = "Historique";
