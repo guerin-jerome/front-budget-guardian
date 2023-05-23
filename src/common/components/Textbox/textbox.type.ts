533e9d7caf2d52561c6361828f3154afe4c59ead
Jérôme Guèrin <guerin_jerome@icloud.com>
2023-05-23 14:46:18 +0200

    feat(history): header history static

--- src/common/components/Textbox/textbox.type.ts
+++ src/common/components/Textbox/textbox.type.ts
@@ -1,6 +1,7 @@
 import { UseFormRegisterReturn } from "react-hook-form";
 
 export type TextboxProps = Partial<{
+  id: string;
   register: UseFormRegisterReturn;
   type: "text" | "number" | "date";
   placeholder: string;
