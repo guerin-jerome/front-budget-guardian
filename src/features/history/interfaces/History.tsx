c1d27de7d2269c449033c50ee57b174c2923dbcc
Jérôme Guèrin <guerin_jerome@icloud.com>
2023-05-23 14:46:18 +0200

    feat(history): header history static

--- src/features/history/interfaces/History.tsx
+++ src/features/history/interfaces/History.tsx
@@ -1,15 +1,28 @@
 import { ExpenseCard } from "./expenseCard/ExpenseCard";
 import { useHistory } from "../hooks/useHistory";
+import { HeaderHistory } from "./headerHistory/HeaderHistory";
+import { Typography } from "@/common/components";
 import "./style.css";
+
+const { Text } = Typography;
 
 export const History = () => {
   const { expensesFormatted } = useHistory();
 
   return (
-    <ul className="expense-list">
-      {expensesFormatted.map((expense) => (
-        <ExpenseCard key={expense.id} {...expense} />
-      ))}
-    </ul>
+    <>
+      {expensesFormatted.length ? (
+        <>
+          <HeaderHistory />
+          <ul className="expense-list">
+            {expensesFormatted.map((expense) => (
+              <ExpenseCard key={expense.id} {...expense} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <Text>Vous n'avez pas encore de dépenses.</Text>
+      )}
+    </>
   );
 };
