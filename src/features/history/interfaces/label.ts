d9d6b36b7314cd8f8eaba6b9f408aab7b72b8782
Jérôme Guèrin <guerin_jerome@icloud.com>
2023-05-23 15:43:10 +0200

    feat(history): header history test + labels

---
+++ src/features/history/interfaces/label.ts
@@ -0,0 +1,4 @@
+export const DETAILS_INPUT_LABEL = "Détails :";
+export const BUDGET_IMPACTED_SELECT_LABEL = "Budget :";
+export const BUDGET_TYPE_SELECT_LABEL = "Type :";
+export const SORT_ORDER_BUDGET_DATE_LABEL = "Trier par date :";
